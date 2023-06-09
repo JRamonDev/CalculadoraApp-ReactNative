32ca3be9aaec3f878abe59016cb08612aa3da705
Jramongs18 <jramongs15@gmail.com>
2023-06-09 17:15:49 -0600

    Importaciones

---
+++ src/Calculadora.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { create, all } from 'mathjs';
+
+export default function Calculadora() {
+    return (
+        <div>Calculadora</div>
+    )
+}
